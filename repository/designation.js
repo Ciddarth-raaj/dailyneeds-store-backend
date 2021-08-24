84a850060c001286f1ec6877606f60ddde145136
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2021-08-25 00:08:10 +0530

    Adds permissions in designation

--- repository/designation.js
+++ repository/designation.js
@@ -7,15 +7,43 @@ class DesignationRepository {
 
   get() {
     return new Promise((resolve, reject) => {
+      this.db.query("SELECT * FROM DESIGNATION", [], (err, docs) => {
+        if (err) {
+          logger.Log({
+            level: logger.LEVEL.ERROR,
+            component: "REPOSITORY.DESIGNATION",
+            code: "REPOSITORY.DESIGNATION.GET",
+            description: err.toString(),
+            category: "",
+            ref: {},
+          });
+          reject(err);
+          return;
+        }
+        resolve(docs);
+      });
+    });
+  }
+
+  create(designation) {
+    return new Promise((resolve, reject) => {
       this.db.query(
-        "SELECT * FROM DESIGNATION",
-        [],
-        (err, docs) => {
+        "INSERT INTO DESIGNATION (status, designation_name, online_portal) VALUES (?, ?, ?)",
+        [
+          designation.status,
+          designation.designation_name,
+          designation.online_portal,
+        ],
+        (err, res) => {
           if (err) {
+            if (err.code === "ER_DUP_ENTRY") {
+              resolve({ code: 101 });
+              return;
+            }
             logger.Log({
               level: logger.LEVEL.ERROR,
               component: "REPOSITORY.DESIGNATION",
-              code: "REPOSITORY.DESIGNATION.GET",
+              code: "REPOSITORY.DESIGNATION.CREATE",
               description: err.toString(),
               category: "",
               ref: {},
@@ -23,42 +51,39 @@ class DesignationRepository {
             reject(err);
             return;
           }
-          resolve(docs)
+          resolve({ code: 200, id: res.insertId });
         }
       );
     });
   }
-  create(designation) {
+
+  createPermission(permission_key, designation_id) {
     return new Promise((resolve, reject) => {
-        this.db.query(
-          "INSERT INTO DESIGNATION (status, designation_name, online_portal) VALUES (?, ?, ?)",
-          [
-            designation.status,
-            designation.designation_name,
-            designation.online_portal
-          ],
-          (err, res) => {
-            if (err) {
-              if (err.code === "ER_DUP_ENTRY") {
-                resolve({ code: 101 });
-                return;
-              }
-              logger.Log({
-                level: logger.LEVEL.ERROR,
-                component: "REPOSITORY.DESIGNATION",
-                code: "REPOSITORY.DESIGNATION.CREATE",
-                description: err.toString(),
-                category: "",
-                ref: {},
-              });
-              reject(err);
+      this.db.query(
+        "INSERT INTO permissions (permission_key, designation_id) VALUES (?, ?)",
+        [permission_key, designation_id],
+        (err, res) => {
+          if (err) {
+            if (err.code === "ER_DUP_ENTRY") {
+              resolve({ code: 101 });
               return;
             }
-            resolve({ code: 200, id: res.insertId });
+            logger.Log({
+              level: logger.LEVEL.ERROR,
+              component: "REPOSITORY.DESIGNATION",
+              code: "REPOSITORY.DESIGNATION.CREATE-PERMISSIONS",
+              description: err.toString(),
+              category: "",
+              ref: {},
+            });
+            reject(err);
+            return;
           }
-        );
-      });
-    }
+          resolve({ code: 200, id: res.insertId });
+        }
+      );
+    });
+  }
 }
 
 module.exports = (db) => {
