74cdfee7c2f417f70d9c4515dfb3a4d0c019b9f4
ciddarthjrj <121765398+ciddarthjrj@users.noreply.github.com>
2025-01-07 13:24:46 +0530

    feat: add telegram_username

--- migrations/mysql/migrations/sqls/20250107063323-outlet-table-up.sql
+++ migrations/mysql/migrations/sqls/20250107063323-outlet-table-up.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `outlets` ADD `telegram_username` VARCHAR(100) NULL DEFAULT NULL AFTER `outlet_nickname`;
+ALTER TABLE `outlets` ADD `opening_cash` FLOAT NOT NULL AFTER `telegram_username`;
