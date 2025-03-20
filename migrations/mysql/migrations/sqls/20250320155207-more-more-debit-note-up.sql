78c69cce91216aa4aa4e9dc4c049b8e49cc9eb18
ciddarthjrj <121765398+ciddarthjrj@users.noreply.github.com>
2025-03-20 21:42:48 +0530

    feat: fix round off

--- migrations/mysql/migrations/sqls/20250320155207-more-more-debit-note-up.sql
+++ migrations/mysql/migrations/sqls/20250320155207-more-more-debit-note-up.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `debit_note_internal` ADD `round_off` FLOAT NOT NULL AFTER `mmh_mrc_refno`;
+ALTER TABLE `debit_note_internal` CHANGE `round_off` `round_off` FLOAT NOT NULL DEFAULT 0.00;
