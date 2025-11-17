26e50d235df638f1174f2340dd79e4015b4748b3
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2025-11-17 13:11:19 +0530

    feat: enhance account validation by allowing null values for no_of_bills and total_sales, improving data processing flexibility

---
+++ migrations/mysql/migrations/sqls/20251024053106-lr-workflow-stage-1-up.sql
@@ -0,0 +1,3 @@
+INSERT INTO `all_permissions` (`permission_key`) VALUES ('create_advance_request');
+INSERT INTO `all_permissions` (`permission_key`) VALUES ('view_advance_request');
+INSERT INTO `all_permissions` (`permission_key`) VALUES ('view_old_balance_check');
