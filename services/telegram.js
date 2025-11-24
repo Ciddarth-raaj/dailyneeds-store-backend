7058851be7e3125caf15749e21ee126b17a4494f
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2025-11-24 12:04:48 +0530

    refactor: restore test mode logic in Telegram message sending for clarity

--- services/telegram.js
+++ services/telegram.js
@@ -15,9 +15,11 @@ class Telegram {
   async sendMessage(chat_id_param, msg, options = {}) {
     let chat_id = chat_id_param;
 
-    // if (process.env.IS_TEST === "true") {
-    //   chat_id = TEST_TELEGRAM_CHAT_ID;
-    // }
+    console.log("ENV VAL", process.env.IS_TEST);
+
+    if (process.env.IS_TEST === "true") {
+      chat_id = TEST_TELEGRAM_CHAT_ID;
+    }
 
     //test-chat-id = 800863889
     return new Promise(async (resolve, reject) => {
