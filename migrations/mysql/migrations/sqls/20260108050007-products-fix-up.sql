532c8ace0caf38fda48c995899a0670faa32b57e
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2026-01-08 15:56:13 +0530

    fix: restore SQL statements for brands and product_images in migration script

--- migrations/mysql/migrations/sqls/20260108050007-products-fix-up.sql
+++ migrations/mysql/migrations/sqls/20260108050007-products-fix-up.sql
@@ -1,6 +1,4 @@
--- TRUNCATE TABLE `brands`
--- ALTER TABLE `brands` ADD PRIMARY KEY(`brand_id`);
-
--- ALTER TABLE `product_images` CHANGE `image_id` `image_id` INT NOT NULL AUTO_INCREMENT;
+TRUNCATE TABLE `brands`
+ALTER TABLE `brands` ADD PRIMARY KEY(`brand_id`);
 
 INSERT INTO `all_permissions` (`permission_key`) VALUES ('edit_products');
