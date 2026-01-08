04641a3e78c2ab436fb5511aaefd8f967b0e134c
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2026-01-08 15:53:36 +0530

    chore: comment out SQL statements for brands and product_images in migration script

--- migrations/mysql/migrations/sqls/20260108050007-products-fix-up.sql
+++ migrations/mysql/migrations/sqls/20260108050007-products-fix-up.sql
@@ -1,6 +1,6 @@
-TRUNCATE TABLE `brands`
-ALTER TABLE `brands` ADD PRIMARY KEY(`brand_id`);
+-- TRUNCATE TABLE `brands`
+-- ALTER TABLE `brands` ADD PRIMARY KEY(`brand_id`);
 
-ALTER TABLE `product_images` CHANGE `image_id` `image_id` INT NOT NULL AUTO_INCREMENT;
+-- ALTER TABLE `product_images` CHANGE `image_id` `image_id` INT NOT NULL AUTO_INCREMENT;
 
 INSERT INTO `all_permissions` (`permission_key`) VALUES ('edit_products');
