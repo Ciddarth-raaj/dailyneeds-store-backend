06351bd4a0c33c455cbffab86b6c7d84b95b9342
Ciddarth Raaj J R J <ciddarthjeyakumar@gmail.com>
2026-05-15 13:23:47 +0530

    refactor: remove redundant sync call from initCronJobs method in Synker class

--- services/synker.js
+++ services/synker.js
@@ -140,7 +140,6 @@ class Synker {
   }
 
   initCronJobs(cronService) {
-    this.syncProductsWithLogging();
     // Schedule CRON job for product sync
     cronService.register("product_sync", CRON_SYNTAX_PRODUCT, async () => {
       await this.syncProductsWithLogging();
