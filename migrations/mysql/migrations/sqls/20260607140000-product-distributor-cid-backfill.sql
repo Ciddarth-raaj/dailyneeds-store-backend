d3a84229e81c0e704aa6c7ed1046b377660cdb35
Ciddarth Raaj J R J <ciddarthjeyakumar@gmail.com>
2026-06-07 20:25:43 +0530

    refactor: update SQL migration to change database reference from dailyneeds-store to dnds_prod for product distributor backfill

--- migrations/mysql/migrations/sqls/20260607140000-product-distributor-cid-backfill.sql
+++ migrations/mysql/migrations/sqls/20260607140000-product-distributor-cid-backfill.sql
@@ -1,5 +1,5 @@
 -- Backfill cid from medishop bridge: mdm_dist_code -> medishopdb_MED_DISTRIBUTOR_MAST.cid
-UPDATE `dailyneeds-store`.`product_distributor` pd
+UPDATE `dnds_prod`.`product_distributor` pd
 INNER JOIN `dailyneeds_gofrugal_sync`.`medishopdb_MED_DISTRIBUTOR_MAST` gf
   ON TRIM(CAST(gf.MDM_DIST_CODE AS CHAR)) = TRIM(CAST(pd.mdm_dist_code AS CHAR))
 SET pd.cid = TRIM(gf.cid)
